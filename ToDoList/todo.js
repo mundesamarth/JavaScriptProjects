3549f41e5343ab75ea2c0b20923d490b5913792d
mundesamarth <munde.samarth28@gmail.com>
2022-04-12 13:49:37 +0530

    Done with the project

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -1,41 +1,58 @@
 add = document.getElementById("addItems");
-add.addEventListener("click",() => {
-    console.log("Updating");
 
-    // local storage
-    tit = document.getElementById('title').value;
-    desc = document.getElementById('description').value;
+function getAndUpdate() {
+  // local storage
+  tit = document.getElementById("title").value;
+  desc = document.getElementById("description").value;
 
-
-    if(localStorage.getItem('jsonItem') == null)
-    {
-        itemsJsonArray = [];
-        itemsJsonArray.push([tit,desc]);
-        localStorage.setItem('jsonItem',JSON.stringify(itemsJsonArray));
-    }
-    else{
-        itemsJsonArrayStr = localStorage.getItem('jsonItem');
-        itemsJsonArray = JSON.parse(itemsJsonArrayStr);
-        itemsJsonArray.push([tit,desc]);
-        localStorage.setItem('jsonItem',JSON.stringify(itemsJsonArray));
-    }
-
-
-    //Adding elements in the table
-    let tableBody = document.getElementById('tableBody');
-    let str = ""
-    itemsJsonArray.forEach((element,index)=>
-    {
-        str += `
+  if (localStorage.getItem("jsonItem") == null) {
+    itemsJsonArray = [];
+    itemsJsonArray.push([tit, desc]);
+    localStorage.setItem("jsonItem", JSON.stringify(itemsJsonArray));
+  } else {
+    itemsJsonArrayStr = localStorage.getItem("jsonItem");
+    itemsJsonArray = JSON.parse(itemsJsonArrayStr);
+    itemsJsonArray.push([tit, desc]);
+    localStorage.setItem("jsonItem", JSON.stringify(itemsJsonArray));
+  }
+  update();
+}
+function update() {
+  if (localStorage.getItem("jsonItem") == null) {
+    itemsJsonArray = [];
+    localStorage.setItem("jsonItem", JSON.stringify(itemsJsonArray));
+  } else {
+    itemsJsonArrayStr = localStorage.getItem("jsonItem");
+    itemsJsonArray = JSON.parse(itemsJsonArrayStr);
+  }
+  console.log("Updating");
+  //Adding elements in the table
+  let tableBody = document.getElementById("tableBody");
+  let str = "";
+  itemsJsonArray.forEach((element, index) => {
+    str += `
         <tr>
-        <th scope="row">${index+1}</th>
+        <th scope="row">${index + 1}</th>
         <td>${element[0]}</td>
         <td>${element[1]}</td>
-        <td><button class="btn btn-sm btn-primary">Delete</button></td>
+        <td><button class="btn btn-sm btn-primary" onclick ="deleted(${index})">Delete</button></td>
     </tr>`;
-    });
-    tableBody.innerHTML = str;
-});
+  });
+  tableBody.innerHTML = str;
+}
+add.addEventListener("click", getAndUpdate);
+update();
 
 // Delete button
-
+function deleted(itemIndex) {
+  console.log("deleted", itemIndex);
+  itemsJsonArrayStr = localStorage.getItem("jsonItem");
+  itemsJsonArray = JSON.parse(itemsJsonArrayStr);
+  itemsJsonArray.splice(itemIndex, 1);
+  localStorage.setItem("jsonItem", JSON.stringify(itemsJsonArray));
+  update();
+}
+function clearStr() {
+  localStorage.clear();
+  update();
+}
